43dfa2c96487c553426067040c44755deb189579
CalebBarbaGarcia <caleb.barba@alumnos.udg.mx>
2020-09-29 17:27:49 -0500

    Se hizo programa calcular e

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var infinito int
+	var e float64
+	var auxMultiplicar int
+
+	fmt.Scan(&infinito)
+
+	for n := 0; n <= infinito; n++ {
+		if n == 0 {
+			e = 1.0
+		} else {
+			auxMultiplicar = 1
+			for aux := n; aux > 0; aux-- {
+				auxMultiplicar = auxMultiplicar * aux
+			}
+			e = e + float64(1/float64(auxMultiplicar))
+		}
+	}
+
+	fmt.Println(e)
 }
